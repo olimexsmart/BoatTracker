b0819dc1c7268d7f84126daf8c8e8bfdf9b470be
Luca Olivieri <gliolivieri@live.it>
2018-06-30 11:41:54 +0200

    Info label what clicking on markers

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 var map;
 var markers = [];
 var flightPath;
+var infoWindow = null;
 
 // Adapting to window size such that both graphs can be displayed
 $("#map").css({ height: $(window).height() * 0.9, width: Math.min($(window).width() * 0.9, 1175) });
@@ -45,21 +46,21 @@ function newRequest(start, end) {
         data: JSON.stringify({ start: start, end: end }),
         timeout: 30000,
         success: function (result) {
-            // Create graphs both for temperature and humidity
-            var result = JSON.parse(result);
-            clearMarkers();
-            var bounds = new google.maps.LatLngBounds();
+            var result = JSON.parse(result); // Decompress JSON into Javascript array
+            clearMarkers();	// Clear map from previous drawings 
+            var bounds = new google.maps.LatLngBounds(); // Object used to set correct zoom level and position
             var path = [];
 
+	    // For each point draw a marker
             result.forEach(point => {
                 var coordinates = { lat: point[1], lng: point[2] };
                 path.push(coordinates);
                 bounds.extend(coordinates);
-                markers.push(new google.maps.Marker({
+                marker = new google.maps.Marker({
                     position: coordinates,
                     map: map,
-                    //animation: google.maps.Animation.DROP,
-                    icon: {
+                    //animation: google.maps.Animation.DROP, // Stupid ah hell
+                    icon: { // Custom icon
                         path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
                         scale: 3,
                         fillColor: 'yellow',
@@ -68,8 +69,21 @@ function newRequest(start, end) {
                         strokeWeight: 1,
                         rotation: point[4] - 180,
                     },
+		    // Extending JS object with custom info to be used in info window 
+		    labelInfo: point[0] + '<br/>Knots: ' + point[3]
+                });
+                 marker.addListener('click', function() {
+                        if (infoWindow != null) // Close if an  object was present
+                                infoWindow.close();
+                        infoWindow = new google.maps.InfoWindow({
+                                content: this.labelInfo
+                        });
 
-                }));
+                        infoWindow.open(map, this);
+                });
+
+                markers.push(marker);
+
             });
 
             flightPath = new google.maps.Polyline({
@@ -93,7 +107,8 @@ function newRequest(start, end) {
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 2,
-        center: { lat: 0, lng: 0 }
+        center: { lat: 0, lng: 0 },
+	streetViewControl: false
     });
 }
 
